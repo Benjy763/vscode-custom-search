5459c93e228b338046d4a08a3684aa9570aab0b0
Benjamin DUPUY <benjamin.dupuy@seloger.com>
2018-12-04 16:36:38 +0100

    Developement of the first version with report in output and local config

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,63 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "migration-state" is now active!');
+    let output: vscode.OutputChannel = vscode.window.createOutputChannel('report');
+    console.log('The extention "migration-state" is active');
+    output.append('Waiting for results...\n');
+    output.show();
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand('extension.generateReport', async () => {
+        let exclusions = `{node_modules,**/*.mock.ts}`;
+        let filesPatterns = [
+            { type: 'AngularJS controllers', number: 0, filesPattern: '**/*.module.js', annotationPattern: /.controller\(/g },
+            { type: 'AngularJS Components', number: 0, filesPattern: '**/*.module.js', annotationPattern: /.component\(/g },
+            { type: 'AngularJS Services', number: 0, filesPattern: `**/*.module.js`, annotationPattern: /.service\(|.factory\(/g },
+            { type: 'AngularJS directives', number: 0, filesPattern: `**/*.module.js`, annotationPattern: /.directive\(/g },
+            { type: 'JS files', number: 0, filesPattern: '**/*.js' },
+            { type: 'Angular components', number: 0, filesPattern: '**/*.ts', annotationPattern: /@Component\(/g },
+            { type: 'Angular services', number: 0, filesPattern: '**/*.ts', annotationPattern: /@Injectable\(/g },
+            { type: 'TS files', number: 0, filesPattern: '**/*.ts' }
+        ];
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        const fileListPromises: any[] = [];
+        filesPatterns.forEach(filePattern => {
+            fileListPromises.push(vscode.workspace.findFiles(filePattern.filesPattern, exclusions));
+        });
+
+        const fileAnnotationSearches: any[] = [];
+        (await Promise.all(fileListPromises)).forEach(async (files: any, index: number) =>{
+            if (!filesPatterns[index].annotationPattern) {
+                filesPatterns[index].number = files.length;
+                fileAnnotationSearches.push(Promise.resolve());
+                return;
+            }
+            const fileContentPromises: any[] = [];
+            files.forEach((uri: any) =>{
+                fileContentPromises.push(vscode.workspace.openTextDocument(uri));
+            });
+            fileAnnotationSearches.push(Promise.all(fileContentPromises));
+        });
+
+        (await Promise.all(fileAnnotationSearches)).forEach((openDocuments: any, index: number) => {
+            if (!openDocuments) {
+                return;
+            }
+            openDocuments.forEach((openDocument: any) => {
+                const fileContentMatches = openDocument.getText().match(filesPatterns[index].annotationPattern);
+                filesPatterns[index].number += fileContentMatches ? fileContentMatches.length : 0;
+            });
+        });
+
+        filesPatterns.forEach(filePattern => {
+            output.append('Number of ' + filePattern.type + ': ' + filePattern.number.toString() + '\n');
+            output.show();
+        });
+        vscode.window.showInformationMessage('Report generated !');
     });
 
     context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
